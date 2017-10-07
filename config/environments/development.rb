2426b15d3e43ef190dc9784f0f3ac70a7a6ecd3c
Deniss <den.b@itservicesgroup.co.uk>
2017-10-07 16:06:20 +0100

    Add default configuration for devise gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,6 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
@@ -28,6 +27,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.perform_caching = false
 
