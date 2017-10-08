ac9e0ab7744ca1c547edd49d20cfbc920c154584
Deniss <den.b@itservicesgroup.co.uk>
2017-10-08 19:23:16 +0100

    Add Seeds data, populate db with users & accounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+puts "Populating db with Users and Accounts, please wait"
+
+1.upto(10) do |n|
+  user = User.create(email: "user_#{n}@example.com", password: 123456, password_confirmation: 123456)
+  Account.create(balance: 100, currency: "eur", user_id: user.id)
+end
+
+puts "Data was populated successfully"
