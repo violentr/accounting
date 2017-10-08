2f88ff6363171194bd5c773a3469e56aa6e24981
Deniss <den.b@itservicesgroup.co.uk>
2017-10-08 18:25:01 +0100

    Add Route "accounts#make_payment"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "accounts#index"
+  post "accounts/make_payment", controller: :accounts, action: :make_payment
   devise_for :users, controllers: {registrations: 'registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
