dc6b5898ad465fbbc08eec81afdd7b54e3d17ef7
Deniss <den.b@itservicesgroup.co.uk>
2017-10-08 19:18:22 +0100

    Add Rspec tests for AccountsController

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,15 @@
 class AccountsController < ApplicationController
   before_action :authenticate_user!
-  def index
 
+  def index
+    @account = current_user.eur_account
+  end
+
+  def make_payment
+    amount = params["transaction"]["amount"].to_i
+    credit_user = User.find_by(id: params["user"].to_i)
+    ::MakePaymentService.new(current_user, credit_user, amount).process!
+
+    redirect_to root_path
   end
 end
--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -2,4 +2,28 @@ require 'rails_helper'
 
 RSpec.describe AccountsController, type: :controller do
 
+  before do
+    @user = User.create(email: "user2@example.com", password: 123456, password_confirmation: 123456)
+    Account.create(balance: 100, currency: "eur", user: @user)
+    sign_in(@user)
+  end
+
+  it "should have respond as :sucess" do
+    get :index
+    expect(response).to have_http_status(:success)
+  end
+
+  it "should not respond as :sucess" do
+    sign_out(@user)
+    get :index
+    expect(response).not_to have_http_status(:success)
+  end
+
+  it "should ceate a payment" do
+    other_user = User.create(email: "user23@example.com", password: 123456, password_confirmation: 123456)
+    Account.create(balance: 100, currency: "eur", user: other_user)
+
+    post :make_payment, params: {user: other_user.id, transaction: {amount: 20}}
+    expect(response).to have_http_status(:redirect)
+  end
 end
