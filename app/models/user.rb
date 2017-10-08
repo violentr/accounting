90015bec9131ab2b375807a04d6cd39daed9c666
Deniss <den.b@itservicesgroup.co.uk>
2017-10-08 17:07:54 +0100

    Add Rspec tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :accounts
+ has_many :accounts
+ scope :all_except, ->(user) { where.not(id: user).map{|u|  [u.email, u.id]} }
+
+ def eur_account
+   accounts.first
+ end
+
+ def eur_account_transactions
+   eur_account.transactions
+ end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   it {is_expected.to respond_to(:email) }
+   it {is_expected.to respond_to(:accounts) }
+
+   before do
+     @user = User.create(email: "user@example.com", password: 123456, password_confirmation: 123456)
+     @account = Account.create(balance: 100, currency: "eur", user: @user)
+   end
+
+   describe "#eur_account" do
+     it "should has Account with currency: :eur" do
+       expect(@user.eur_account).to eq(@account)
+     end
+   end
+
+   describe "#eur_account_transactions" do
+     it "should has no transactions for :eur account" do
+       expect(@user.eur_account_transactions).to be
+     end
+   end
+
+   describe ".all_except" do
+     it "should not include defined user" do
+       1.upto(5) do |n|
+         User.create(email: "user#{n}@example.com")
+       end
+       expect(described_class.all_except(@user)).not_to include(@user)
+     end
+   end
 end
