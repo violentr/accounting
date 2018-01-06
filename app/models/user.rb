aa68ef1366d5623ce6efa60b1d5eb2afe21011fe
Deniss <den.b@itservicesgroup.co.uk>
2018-01-06 22:56:25 +0000

    Add User#make_payment
    User can make make payments

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
- has_many :accounts
- scope :all_except, ->(user) { where.not(id: user).map{|u|  [u.email, u.id]} }
+  has_many :accounts
+  scope :all_except, ->(user) { where.not(id: user).map{|u|  [u.email, u.id]} }
 
- def eur_account
-   accounts.first
- end
+  def eur_account
+    accounts.first
+  end
 
- def eur_account_transactions
-   eur_account.transactions
- end
+  def eur_account_transactions
+    eur_account.transactions
+  end
+
+  def make_payment(payment)
+    payment.process!
+  end
 end
